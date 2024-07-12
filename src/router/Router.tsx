9ff17944a50d9f5255e4bb845ba47b48bef4a60b
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2024-07-12 11:13:34 +0530

    +profile icon ±router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import Order from "../pages/Order";
-import Home from "../pages/Home";
+import Home from "../pages/Dashboard";
+import { useLocation } from "react-router-dom";
+
+const NotFound = () => {
+  const location = useLocation();
+  return (
+    <div
+      style={{
+        height: "80vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {location.pathname}
+    </div>
+  );
+};
 
 export const router = createBrowserRouter([
   {
@@ -9,7 +26,12 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { path: "", element: <Home /> },
-      { path: "order", element: <Order /> },
+      { path: "/dashboard", element: <Home /> },
+      { path: "/order-management", element: <Order /> },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
     ],
   },
 ]);
