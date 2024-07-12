b63eca78d749bfd6b107a3be24c5c80d583a6658
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2024-07-12 11:12:58 +0530

    +styled comps

---
+++ src/components/styled/index.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Header = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  font-weight: bold;
+  color: #1f2937;
+`;
+
+export const ContainerWrapper = styled.div`
+padding-block: 1rem;
+background-color: #f9fafb;
+min-height: 100vh;
+`;
+
+export const Title = styled.h2`
+font-size: 18px;
+`;
+
+export const Subtitle = styled.p`
+color: grey;
+`;
+
+export const NumValue = styled.div`
+font-size: 32px;
+font-weight: bold;
+`;
