d2e43951d2d027e23da840752d0dafd16331ee54
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2024-07-12 11:09:46 +0530

    ±layout :fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,19 @@
-
-import { Outlet } from 'react-router-dom';
-import SidePanel from './SidePanel';
+import { Outlet } from "react-router-dom";
+import SidePanel from "./SidePanel";
+import Navbar from "./Navbar";
+import { useState } from "react";
 
 function Layout() {
+  const [isCollapsed, setisCollapsed] = useState(false);
+
   return (
-    <div style={{ display: 'flex' }}>
-      <SidePanel />
-      <main style={{ marginLeft: '200px' }}>
+    <div style={{ display: "flex", position: "relative", width: "100%",maxWidth:"100vw",overflow: "hidden" }}>
+      <SidePanel isCollapsed={isCollapsed} setIsCollapsed={setisCollapsed} />
+      <div style={{ minHeight: "100vh", minWidth:`${isCollapsed ? "100px" : "260px"}`,width: `${isCollapsed ? "10%" : "15%"}` }}></div>
+      <main
+        style={{ minHeight: "100vh", width:`${!isCollapsed ? "85%" : "90%"}` ,padding: "20px",transition: "width 0.5s"}}
+      >
+        <Navbar />
         <Outlet />
       </main>
     </div>
